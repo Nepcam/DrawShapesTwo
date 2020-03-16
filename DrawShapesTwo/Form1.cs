b579cb27a415c3b907c1f4df6eb0cd650bd24dcc
Nepcam <cameron.nepe@gmail.com>
2020-03-16 22:23:20 +1300

    Add project files.

---
+++ DrawShapesTwo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawShapesTwo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            Pen pen1 = new Pen(Color.Red, 2);
+
+            paper.DrawLine(pen1, 600, 300, e.X, e.Y);
+        }
+    }
+}
