1c61619c9cba588950a4e0adccb833efd52c1e87
Nepcam <cameron.nepe@gmail.com>
2020-05-16 14:11:08 +1200

    DrawEllipse() and FillEllipse() methods

--- DrawShapesTwo/Form1.Designer.cs
+++ DrawShapesTwo/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.buttonSetColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,16 +39,28 @@
             this.pictureBoxDisplay.BackColor = System.Drawing.Color.DarkGray;
             this.pictureBoxDisplay.Location = new System.Drawing.Point(1, 0);
             this.pictureBoxDisplay.Name = "pictureBoxDisplay";
-            this.pictureBoxDisplay.Size = new System.Drawing.Size(1232, 614);
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(1232, 518);
             this.pictureBoxDisplay.TabIndex = 0;
             this.pictureBoxDisplay.TabStop = false;
             this.pictureBoxDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxDisplay_MouseMove);
+            // 
+            // buttonSetColor
+            // 
+            this.buttonSetColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSetColor.Location = new System.Drawing.Point(48, 538);
+            this.buttonSetColor.Name = "buttonSetColor";
+            this.buttonSetColor.Size = new System.Drawing.Size(164, 52);
+            this.buttonSetColor.TabIndex = 1;
+            this.buttonSetColor.Text = "Set Color";
+            this.buttonSetColor.UseVisualStyleBackColor = true;
+            this.buttonSetColor.Click += new System.EventHandler(this.buttonSetColor_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1235, 615);
+            this.Controls.Add(this.buttonSetColor);
             this.Controls.Add(this.pictureBoxDisplay);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -59,6 +73,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.Button buttonSetColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- DrawShapesTwo/Form1.cs
+++ DrawShapesTwo/Form1.cs
@@ -12,6 +12,7 @@ namespace DrawShapesTwo
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +20,31 @@ namespace DrawShapesTwo
 
         private void pictureBoxDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            Graphics canvas = pictureBoxDisplay.CreateGraphics();
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
             Pen redPen = new Pen(Color.Red, 2);
             Pen bluePen = new Pen(Color.Blue, 2);
+            Pen rainbowPen = new Pen(colorDialog1.Color);
 
-            canvas.DrawLine(redPen, pictureBoxDisplay.Width/2, pictureBoxDisplay.Height/2, e.X, e.Y);
+            //paper.DrawLine(rainbowPen, pictureBoxDisplay.Width/2, pictureBoxDisplay.Height/2, e.X, e.Y);
             //canvas.DrawLine(bluePen, 0, 0, e.X, e.Y);
+
+            // IF the button is pressed THEN
+            if (e.Button == MouseButtons.Left)
+            {
+                //FILL shape with color
+                SolidBrush br = new SolidBrush(Color.Orange);
+                //DRAW the line
+                //paper.DrawLine(rainbowPen, rand.Next(pictureBoxDisplay.Width +1), pictureBoxDisplay.Height +1, e.X, e.Y);
+                //DRAW circle
+                paper.DrawEllipse(bluePen, e.X, e.Y, 50, 50);
+                //FILL circle
+                paper.FillEllipse(br, e.X, e.Y, 50, 50);
+            }
+        }
+
+        private void buttonSetColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();           
         }
     }
 }
